c103a7984b0dfb5bbd8f69df579a028ed671a9ee
todorm85 <todorbm85@gmail.com>
2015-01-18 22:14:31 +0200

    added 02.Data Types with gitignore

---
+++ C# part 1/02 Data Types and Variables/11. Bank Account Data/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class BankAccountData
+{
+    static void Main()
+    {
+        string firstName = "";
+        string middleName = "";
+        string lastName = "";
+        //ok, so for the names we use string because they are just an array of characters
+        decimal balance = 0;
+        //we use decimal for the account balance because it is required for financial
+        //calculations due to its accuracy when working with decimal numbers
+        string bankName = "";
+        //the bank name is just another array of characters so string is used
+        string IBAN = "";
+        string card1 = "";
+        string card2 = "";
+        string card3 = "";
+        //the iban and credit card numbers are also defined as array of characters
+        //because they probably won`t participate in any calculations
+
+    }
+}
+
---
+++ C# part 1/02 Data Types and Variables/6. Strings and Objects/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class StringsObjects
+{
+    static void Main()
+    {
+        string hello = "Hello";
+        string world = "World";
+        object phrase = hello + " " + world;
+        string greeting = (string)phrase;
+        Console.WriteLine(greeting);
+    }
+}
+
