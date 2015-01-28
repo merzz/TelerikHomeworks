72ce546baf0d55c673c9055cb1e3ff914540395c
todorm85 <todorbm85@gmail.com>
2015-01-28 11:47:17 +0200

    add 17,18 to Loops

--- C# part 1/06 Loops/17. Calculate GCD/17. Calculate GCD.cs
+++ C# part 1/06 Loops/17. Calculate GCD/17. Calculate GCD.cs
@@ -15,6 +15,35 @@ class Calculate_GCD
 {
     static void Main()
     {
+        int[] nums = new int[2];
 
+        for (int i = 0; i < 2; i++)
+        {
+            Console.Write("Enter integer {0}: ", i + 1);
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        if (nums[1] > nums[0])  //swap the numbers if second is bigger
+        {
+            nums[0] += nums[1];
+            nums[1] = nums[0] - nums[1];
+            nums[0] -= nums[1];
+        }
+
+        int numA = nums[0];
+        int numB = nums[1];
+        int del = 0;
+        int gcd = numA % numB;
+
+        do
+        {
+            del = numA % numB;
+            gcd = (gcd > del) ? gcd : del;
+            numA = numB;
+            numB = del;
+        }
+        while (del != 0);
+
+        Console.WriteLine(gcd);
     }
 }
--- C# part 1/06 Loops/18. Trailing Zeroes in N!/18. Trailing Zeroes in N!.cs
+++ C# part 1/06 Loops/18. Trailing Zeroes in N!/18. Trailing Zeroes in N!.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 //Problem 18.* Trailing Zeroes in N!
 //• Write a program that calculates with how many zeroes the factorial
@@ -13,8 +14,57 @@
 
 class Trailing_Zeroes_in_Nfact
 {
+    //static void Main()
+    //{
+    //start:
+    //    Console.Write("Enter integer: ");
+    //    ulong num = ulong.Parse(Console.ReadLine());
+    //    BigInteger factorial = 1;
+
+    //    do
+    //    {
+    //        factorial *= num;
+    //        num--;
+    //    }
+    //    while (num != 0);
+
+    //    uint i = 0;
+    //    BigInteger remainder = factorial % 10;
+
+    //    if (factorial > 24)    //because the cycle doesn`t work for facotrials less than 5 and anyway they don`t have trailing zeros
+    //    {
+    //        while (remainder == 0)
+    //        {
+    //            i++;
+    //            remainder = factorial % 10;
+    //            factorial /= 10;
+    //        }
+    //        i--;
+    //    }
+    //    Console.WriteLine("Trailing zeros: " + i);
+    //    goto start;
+    //}
+
     static void Main()
     {
+        //of course theres a simpler method than brute force calculation
+        //if you divide the original number by 5, the first result is trailing zeros from all 5s (5^1),
+        //then divide again this result by 5, you get the zeros from all the 25s (5^2) and so on until you get zero result
+        //for example if you try to calculate !101. You need to find all the 5s in it. it is easy 101/5 = 20.
+        //So you have 20 trailing digits. But we missed 25s. They are powers of 5 which will also give trailing zeros.
+        //Just delete 101/25 = 4. 
+    start:
+        Console.Write("Enter integer: ");
+        ulong num = ulong.Parse(Console.ReadLine());
+        ulong zeroCount = 0;
 
+        do
+        {
+            zeroCount += num / 5;
+            num = num / 5;
+        }
+        while (num >= 5);
+        Console.WriteLine(zeroCount);
+        goto start;
     }
 }
