9622e011072cda8e3d79709db6055e8a0deee073
todorm85 <todorbm85@gmail.com>
2015-02-06 13:23:06 +0200

    added arrays hw

---
+++ C# part 2/01. Arrays/13. Merge sort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//### Problem * 13. Merge sort
+//*	Write a program that sorts an array of integers using the [Merge sort](http://en.wikipedia.org/wiki/Merge_sort) algorithm (find it in Wikipedia).
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter array (members delimited by space: ");
+        string[] arrStr = Console.ReadLine().Split(' ');
+        int[] arr = new int[arrStr.Length];
+        for (int i = 0; i < arrStr.Length; i++)
+        {
+            arr[i] = int.Parse(arrStr[i]);
+        }
+
+
+    }
+}
+
---
+++ C# part 2/01. Arrays/20. Variations of set/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//### Problem 20. Variations of set
+//*	Write a program that reads two numbers `N` and `K` and generates all the variations of `K` elements from the set [`1..N`].
+class Program
+{
+    static void Main()
+    {
+        // doesn`t work when k > 2
+        Console.Write("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = i;
+        }
+
+        Console.Write("Enter K: ");
+        int k = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] +" ");
+
+            int counter = 1;
+
+            for (int ii = 0; ii < n; ii++)
+            {
+                if (ii == n - 1 && i == n - 1) break;
+                if (counter % k == 0 ) { Console.Write("\n" + arr[i] + " "); counter++;}
+                if (ii != i) { Console.Write(arr[ii] + " "); counter++; }
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
+
